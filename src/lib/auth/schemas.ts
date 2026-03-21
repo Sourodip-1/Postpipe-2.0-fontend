dd00a06b39bbf72fb14ede192604316126f0b130
souvikvos <souvikgh.work@gmail.com>
2026-03-21 21:01:09 +0530

    feat: Add Zod schemas for user authentication, password management, email verification, and profile updates.

--- src/lib/auth/schemas.ts
+++ src/lib/auth/schemas.ts
@@ -4,10 +4,10 @@ export const SignupSchema = z.object({
     name: z.string().min(2, { message: "Name must be at least 2 characters long." }),
     email: z.string().email({ message: "Please enter a valid email." }).trim(),
     password: z.string()
-        .min(8, { message: "Be at least 8 characters long" })
-        .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
-        .regex(/[0-9]/, { message: "Contain at least one number." })
-        .regex(/[^a-zA-Z0-9]/, { message: "Contain at least one special character." })
+        .min(8, { message: "Must be at least 8 characters long, contain a letter, a number, and a special character." })
+        .regex(/[a-zA-Z]/, { message: "Must be at least 8 characters long, contain a letter, a number, and a special character." })
+        .regex(/[0-9]/, { message: "Must be at least 8 characters long, contain a letter, a number, and a special character." })
+        .regex(/[^a-zA-Z0-9]/, { message: "Must be at least 8 characters long, contain a letter, a number, and a special character." })
         .trim(),
     confirmPassword: z.string()
 }).refine((data) => data.password === data.confirmPassword, {
